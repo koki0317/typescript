51914d20a58c9009e0feee0a60b7c5be1866b7f2
koki0317 <80045896+koki0317@users.noreply.github.com>
2022-09-22 09:50:50 +0900

    Fixed errorboundary and carousel

--- src/Details.tsx
+++ src/Details.tsx
@@ -21,6 +21,10 @@ class Details extends Component<{ params: {id?: string}}> {
   };
 
   async componentDidMount() {
+    if (!this.props.params.id) {
+      return;
+    }
+
     const res = await fetch(
       `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
     );
